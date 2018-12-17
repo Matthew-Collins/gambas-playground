f7b7d65fc828abf62ee74ca83d991150d8a80bcd
Matthew Collins <matthew.collins@cwgsy.net>
2018-12-17 08:39:37 +0000

    New Server App written in php

---
+++ playground-php-server/README.md
@@ -0,0 +1,12 @@
+# To Run on Apache Web Server
+
+Add User to existing Docker Group:
+
+sudo usermod -aG docker www-data
+
+
+Copy run.php to: 
+
+/var/www/html/playground
+
+
---
+++ playground-php-server/run.php
@@ -0,0 +1,22 @@
+<?php
+
+    // Get POST data
+    $code = file_get_contents('php://input');
+
+    // Create Container
+    $container = exec('docker create --cap-drop=ALL --cpus=1 --memory=16m --net=none --pids-limit=20 --security-opt=no-new-privileges --interactive gbs3');
+
+    // Save Code to Script File
+    file_put_contents('/scripts/' . $container . '.gbs', $code);  
+
+    // Copy Script into Container
+    exec('docker cp /scripts/' . $container . '.gbs ' . $container . ':script.gbs');
+
+    // Delete Script File
+    unlink('/scripts/' . $container . '.gbs');
+
+    // Start Container 
+    $output = exec('docker start --attach --interactive ' . $container);
+    echo $output;
+
+?>
