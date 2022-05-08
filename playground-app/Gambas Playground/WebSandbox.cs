8293bd392da82574342396a4da64e0513a0d0a35
Matthew Collins <matthew.collins@cwgsy.net>
2022-05-08 13:12:00 +0100

    New Icon and Splash Screen Fix Issue with LF Not displaying on Android Devices

--- playground-app/Gambas Playground/WebSandbox.cs
+++ playground-app/Gambas Playground/WebSandbox.cs
@@ -16,24 +16,36 @@ namespace Gambas_Playground
         {
 
             // Clean Line Endings
-            Code = Code.Replace(Convert.ToString(Strings.Chr(13) + Strings.Chr(10)), Convert.ToString(Strings.Chr(10)));
-            Code = Code.Replace(Strings.Chr(13), Strings.Chr(10));
-            
+            Code = Code.Replace(Constants.vbCrLf, Constants.vbLf);
+            Code = Code.Replace(Constants.vbCr, Constants.vbLf);
+
             // Clean 66, 99 Quotes
             Code = Code.Replace(Strings.ChrW(8220), Strings.Chr(34));
             Code = Code.Replace(Strings.ChrW(8221), Strings.Chr(34));
-            
+
             // Run Code in Web Sandbox
+            string result = string.Empty;
             var Client = new HttpClient();
-            var Response = await Client.PostAsync(Host, new StringContent(Code));
-            if (Response.IsSuccessStatusCode)
+            try
             {
-                return (await Response.Content.ReadAsStringAsync()).Trim(Conversions.ToChar(Constants.vbLf));
+                var Response = await Client.PostAsync(Host, new StringContent(Code));
+                if (Response.IsSuccessStatusCode)
+                {
+                    result = (await Response.Content.ReadAsStringAsync());
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return string.Empty;
+                return ex.Message;
             }
+
+            // Clean Line Endings
+            result = result.Trim(Conversions.ToChar(Constants.vbLf));
+            result = result.Replace(Constants.vbCrLf, Constants.vbLf);
+            result = result.Replace(Constants.vbCr, Constants.vbLf);
+            result = result.Replace(Constants.vbLf, Constants.vbCrLf);
+
+            return result;
 
         }
 
